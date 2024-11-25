40426f5b098f335130fc129f78935653dff01d12
Fabian-2596 <fabianschmidt9625@gmx.de>
2024-11-25 23:23:55 +0100

    ratesystem und punktestand hinzugefügt

--- PokemonGuesser.py
+++ PokemonGuesser.py
@@ -4,8 +4,10 @@ from nicegui import ui
 import random
 
 pokemonList = []
-pokemonImages = []
 points = 0
+input = ""
+currentPokemon = ""
+
 
 url = "https://www.pokewiki.de/Pok%C3%A9mon-Liste"
 page = urlopen(url)
@@ -14,25 +16,41 @@ html = html_bytes.decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
 
 td = soup.find_all("td")
-img = soup.find_all("img")
-count = 3
-imgcount = 0
+count = 2
 size = len(td)
 while (count < size):
-    pokemonImages.append(img[imgcount]["src"])
     pokemonList.append(td[count].string)
-    count += 9 
-    imgcount += 1
+    count += 9
 
 @ui.refreshable
-def newPokemon(img):
-    siteurl = "https://www.pokewiki.de"
-    index = random.randrange(len(img))
-    imgurl = siteurl + pokemonImages[index]
+def newPokemon():
+    index = random.randrange(len(pokemonList))
+    global currentPokemon
+    currentPokemon = pokemonList[index]
+    print(currentPokemon)
+    url = "https://www.pokewiki.de/" + currentPokemon
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    table = soup.find_all('td', attrs={'colspan': "2"})
+    for x in table:
+        ref = x.find('a', class_="mw-file-description")
+        if(ref != None):
+            break
+
+    image_tag = ref.find('img')
+    imagelink = "https://www.pokewiki.de/" + image_tag["src"]
     ui.label(points)
-    ui.image(imgurl).style("width: 25%").style("height: 25%").style("")
+    ui.image(imagelink).style("width: 25%").style("height: 25%").style("")
 
+def checkAnswer():
+    global points
+    if(textInput.value.lower() == currentPokemon.lower()):
+        points += 1
+    newPokemon.refresh()
 
-newPokemon(img)
-ui.button("next", on_click=newPokemon.refresh)
-#ui.run()
+newPokemon()
+textInput = ui.input(label="Welches Pokemon ist das?")
+ui.button("Rate!", on_click=checkAnswer)
+ui.run()
